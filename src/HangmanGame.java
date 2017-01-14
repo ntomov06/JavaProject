af5cc0f53b270af7b0ec4c15d32c1e5993755ade
miletomova <mile.tomova@gmail.com>
2017-01-14 18:47:53 +0200

    Almost done , without simple cases , and library of cities.

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -1,60 +1,85 @@
 import java.util.Scanner;
 import java.util.Random;
 
+//Methods we need 
+// generating a random number
+//1st method for choosing a word
+//2nd method for inputing a players guess
+//3rd comparing the chosen word with with letters that players inputs
+// 4thmethod for drawing a picture of hangedman and counting lefted tries
+//
 public class HangmanGame {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Random random = new Random();
 		System.out.println("Enter your name.");
 		String name = scanner.nextLine();
-		String[] guesses = { "asd", "dsadsa", "dwqds", "sad" };
 		boolean areWePlaying = true;
 		while (areWePlaying) {
 			System.out.printf("Welcome to my Hangman game , %s. Let's the game begin.", name);
 			System.out.println();
-			char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
-			int amountOfGuesses = randomWordToGuess.length;
-			char[] playerGuess = new char[amountOfGuesses];
+			System.out.println("___________________________________________");
+			System.out.println("First I will tell you the rules and commands for the game.");
+			System.out.println("After you entered your name and started playing , I will generate a random word that you must guess.");
+			System.out.println("Word that are in this game are only cities form our country, Bulgaria.");
+			System.out.println("You have 6 wrong tries and once you reach the maximum , you are getting hanged and loose the game!");
+			System.out.println("If you want to quit the game and stop playing , just press \"-\" and the game will stop.");
+			System.out.println("Good luck , have fun!");
+			System.out.println("___________________________________________");
+			char[] randomWordToGuess = chosingWord();
+			int amountOfFalseGuesses = 0;
+			char[] playerGuess = new char[randomWordToGuess.length + amountOfFalseGuesses];
 			for (int i = 0; i < playerGuess.length; i++) {
 				playerGuess[i] = '_';
 			}
 			boolean wordIsGuessed = false;
-			int tries = 0;
-			while (!wordIsGuessed && tries != amountOfGuesses) {
+			while (!wordIsGuessed && amountOfFalseGuesses < 6) {
 				System.out.print("Current guesses :");
 				printArray(playerGuess);
-				System.out.printf("You have %d tries left.\n" , amountOfGuesses-tries);
 				System.out.println("Enter a letter");
 				char input = scanner.nextLine().charAt(0);
-				tries++;
-				if (input=='-'){
-					areWePlaying=false;
-					wordIsGuessed=true;
+				if (input == '-') {
+					areWePlaying = false;
+					wordIsGuessed = true;
 				} else {
-					for (int i=0;i<randomWordToGuess.length;i++){
-						if (randomWordToGuess[i]==input){
-							playerGuess[i]=input;
+					for (int i = 0; i < randomWordToGuess.length; i++) {
+						if (randomWordToGuess[i] == input) {
+							playerGuess[i] = input;
 						}
 					}
-				}
-				if (isTheWordGuessed(playerGuess)){
-					wordIsGuessed = true ;
-					System.out.println("Congrats, you won!");
-					areWePlaying=false;
+					if (isSearchingWordContainsInput(randomWordToGuess, input) == false) {
+						amountOfFalseGuesses++;
+					}
+					;
+					drawingHangman(amountOfFalseGuesses);
+					if (isTheWordGuessed(playerGuess)) {
+						wordIsGuessed = true;
+						System.out.println("Congrats, you won!");
+						areWePlaying = false;
+					}
 				}
 			}
-			if (!wordIsGuessed){
-				System.out.println("You ran out of guesses.");
+			System.out.println("Do you want to play another game? (yes/no)");
+			String anotherGame = scanner.nextLine();
+			if (anotherGame.equals("no")) {
+				System.out.println("Game over.Come back later, " + name + ".");
 				areWePlaying=false;
 			}
 		}
-		System.out.println("Do you want to play another game? (yes/no)");
-		String anotherGame=scanner.nextLine();
-		if (anotherGame.equals("yes")){
-			areWePlaying=true;
-		}
-		System.out.println("Game over.");
+	}
+
+	public static char[] chosingWord() {
+
+		String[] guesses = { "Vratsa", "Mezdra", "Sofia", "Veliko Turnovo" };
+
+		Random random = new Random();
+		char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray(); // choosing
+																							// a
+																							// random
+																							// word
+																							// form
+																							// list
+		return randomWordToGuess;
 	}
 
 	public static void printArray(char[] array) {
@@ -63,12 +88,103 @@ public class HangmanGame {
 		}
 		System.out.println();
 	}
-	public static boolean isTheWordGuessed(char[] array){
+
+	public static boolean isTheWordGuessed(char[] array) {
 		for (int i = 0; i < array.length; i++) {
-			if (array[i]=='_'){
+			if (array[i] == '_') {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public static boolean isSearchingWordContainsInput(char[] guesses, char input) {
+		boolean isContains = false;
+		int timesOfAppears = 0;
+		for (int i = 0; i < guesses.length; i++) {
+			if (guesses[i] == input) {
+				timesOfAppears++;
+			}
+		}
+		if (timesOfAppears > 0) {
+			isContains = true;
+		}
+		return isContains;
+	}
+
+	public static void drawingHangman(int amountOfFailTries) {
+		if (amountOfFailTries == 1) {
+			firstFailTry();
+		} else if (amountOfFailTries == 2) {
+			secondFailTry();
+		} else if (amountOfFailTries == 3) {
+			thirdFailTry();
+		} else if (amountOfFailTries == 4) {
+			fourthFailTry();
+		} else if (amountOfFailTries == 5) {
+			fifthFailTry();
+		} else if (amountOfFailTries == 6) {
+			sixthFailTry(chosingWord().toString());
+		}
+	}
+
+	public static void firstFailTry() {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("You have 4 fail tries left!");
+	}
+
+	public static void secondFailTry() {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|     |");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("You have 3 fail tries left!");
+	}
+
+	public static void thirdFailTry() {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|    \\|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("You have 2 fail tries left!");
+	}
+
+	public static void fourthFailTry() {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|    \\|/");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("You have 1 fail tries left!");
+	}
+
+	public static void fifthFailTry() {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|    \\|/");
+		System.out.println("|    /");
+		System.out.println("|");
+		System.out.println("You have 0 fail tries left!One more and you loose.");
+	}
+
+	public static void sixthFailTry(String randomGuessingWord) {
+		System.out.println(" _____");
+		System.out.println("|/    |");
+		System.out.println("|     o");
+		System.out.println("|    \\|/");
+		System.out.println("|    / \\");
+		System.out.println("|");
+		System.out.println("You lost.The word was..." + randomGuessingWord);
 	}
 }
